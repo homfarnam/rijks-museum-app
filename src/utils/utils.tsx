0d18a109683b74a20a4c9fed9d2db16ab086bd09
homfarnam <homfarnam@gmail.com>
2022-11-20 20:13:22 +0100

    Add utils

---
+++ src/utils/utils.tsx
@@ -0,0 +1,19 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const createTestQueryClient = () =>
+  new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  })
+
+export const createWrapper = () => {
+  const testQueryClient = createTestQueryClient()
+  return ({ children }: { children: React.ReactNode }) => (
+    <QueryClientProvider client={testQueryClient}>
+      {children}
+    </QueryClientProvider>
+  )
+}
