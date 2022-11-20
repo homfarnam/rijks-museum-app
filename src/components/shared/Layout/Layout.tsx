b3825e189d13c51741f4934305e5b676f552b0a2
homfarnam <homfarnam@gmail.com>
2022-11-20 12:50:29 +0100

    Add layout component

---
+++ src/components/shared/Layout/Layout.tsx
@@ -0,0 +1,17 @@
+interface LayoutProps {
+  children: React.ReactNode
+  title: string
+}
+
+const Layout = ({ children, title }: LayoutProps) => {
+  return (
+    <div className="layout">
+      <header className="layout--title">
+        <h1>{title}</h1>
+      </header>
+      <main>{children}</main>
+    </div>
+  )
+}
+
+export default Layout
