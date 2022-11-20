f333de7db231e8d70198cb856ab58029764a272d
homfarnam <homfarnam@gmail.com>
2022-11-20 12:54:31 +0100

    Add components index file

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import ArtPiece from './ArtPiece/ArtPiece'
+import OptimizedImage from './OptimizedImage/OptimizedImage'
+import Paints from './Paints/Paints'
+import Layout from './shared/Layout/Layout'
+
+export { Paints, Layout, ArtPiece, OptimizedImage }
