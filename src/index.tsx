6f4f46fdbb0b0d05c2cf8ce97f0bb9fedc6d52f2
homfarnam <homfarnam@gmail.com>
2022-11-20 12:50:18 +0100

    Use react query provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import "./styles/tailwind.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import React from 'react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import App from './App'
+import './styles/tailwind.css'
+import './styles/main.scss'
+import 'react-lazy-load-image-component/src/effects/blur.css'
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 3000 } },
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </React.StrictMode>
+  </QueryClientProvider>
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
